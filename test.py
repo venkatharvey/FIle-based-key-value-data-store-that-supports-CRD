93a6737803027331af10f3c5628284bfeef30fe9
venkatharvey <72544994+venkatharvey@users.noreply.github.com>
2020-12-28 09:32:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import srccode as s
+import time
+import threading
+from threading import *
+
+t1=Thread(target=s.create,args=("venkat",21))
+t1.start()
+time.sleep(3)
+#creation of a new key-value pair in the datastore
+
+t2=Thread(target=s.create,args=("vignesh",22))
+t2.start()
+time.sleep(3)
+#creation of another key-value pair in the data store
+
+t3=Thread(target=s.read,args=("venkat",))
+t3.start()
+print(s.read("venkat"))
+time.sleep(3)
+#reads the value of the key and returns the result in the json format
+
+t4=Thread(target=s.delete,args=("venkat",))
+t4.start()
+time.sleep(3)
+#deletes the key-value pair
